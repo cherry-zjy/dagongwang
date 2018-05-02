0d512b44f5405b6502084ecce3c84b4bf886bc13
zjy <296579033@qq.com>
2018-05-02 16:58:39 +0800

    主页页面初步搭建

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,6 +20,28 @@ Page({
       Address:'宁波市鄞州区',
       Money:'200元',
       Price:'3500元'
+    },{
+      ID: 1,
+      Display_Image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      Type: '补贴',
+      Address: '宁波市鄞州区',
+      Money: '200元',
+      Price: '3500元'
+    },
+    {
+      ID: 1,
+      Display_Image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      Type: '补贴',
+      Address: '宁波市鄞州区',
+      Money: '200元',
+      Price: '3500元'
     }]
+  },
+  // 点击跳转详情页
+  jobDetail(event) {
+    var id = event.currentTarget.dataset.id
+    wx.navigateTo({
+      url: 'newsDetail/newsDetail?id=' + id,
+    })
   },
 })
