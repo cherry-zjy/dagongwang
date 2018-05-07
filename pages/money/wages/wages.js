9434d8390de10a4f2ca352a00f926f0b7ef91529
zjy <296579033@qq.com>
2018-05-07 15:36:56 +0800

    线下门店详情页面初步完成

--- pages/money/wages/wages.js
+++ pages/money/wages/wages.js
@@ -1,29 +1,11 @@
-// pages/money/store/storedetail/storedetail.js
+// pages/money/wages/wages.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000,
-    store:{
-      Name: '苏州马涧集散',
-      WorkTime: '周一到周五 09:00~18:00',
-      Address: '江苏省苏州市虎丘区朝红路168号',
-      Distance: "12868公里",
-      Agent: "学姐",
-      Phone: "18258773565",
-      Type:0,
-      Icon:'../../../../img/icon.png'
-    }
+  
   },
 
   /**
