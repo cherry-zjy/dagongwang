61f8ec4781063a894df1938f510eec0c520be0c9
zjy <296579033@qq.com>
2018-05-08 11:45:20 +0800

    关注列表页面初步完成

--- pages/mine/follow/follow.js
+++ pages/mine/follow/follow.js
@@ -5,6 +5,30 @@ Page({
    * 页面的初始数据
    */
   data: {
+    none:false,
+    jobList: [{
+      ID: 1,
+      Display_Image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      Type: '补贴',
+      Address: '宁波市鄞州区',
+      Money: '200元',
+      Price: '3500元'
+    }, {
+      ID: 1,
+      Display_Image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      Type: '补贴',
+      Address: '宁波市鄞州区',
+      Money: '200元',
+      Price: '3500元'
+    },
+    {
+      ID: 1,
+      Display_Image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      Type: '补贴',
+      Address: '宁波市鄞州区',
+      Money: '200元',
+      Price: '3500元'
+    }],
   
   },
 
@@ -26,7 +50,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    var that = this;
+    if (that.data.jobList.length == 0){
+      that.setData({
+        none: true
+      })
+    }
   },
 
   /**
