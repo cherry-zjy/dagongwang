696c651987960d3bcb822901c5ea60bf29975f05
zjy <296579033@qq.com>
2018-05-10 14:12:35 +0800

    首页接口完成，包括叠层，20个分组

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,37 +6,81 @@ Page({
   data: {
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
     indicatorDots: true,
     autoplay: true,
     interval: 5000,
     duration: 1000,
-    jobList:[{
-      ID:1,
-      Display_Image:'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
-      Type:'补贴',
-      Address:'宁波市鄞州区',
-      Money:'200元',
-      Price:'3500元'
-    },{
-      ID: 1,
-      Display_Image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
-      Type: '补贴',
-      Address: '宁波市鄞州区',
-      Money: '200元',
-      Price: '3500元'
-    },
-    {
-      ID: 1,
-      Display_Image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
-      Type: '补贴',
-      Address: '宁波市鄞州区',
-      Money: '200元',
-      Price: '3500元'
-    }],
-    searchVal:''
+    jobList1: [],
+    jobList2: [],
+    jobList3: [],
+    jobList4:[],
+    searchVal:'',
+    isscroll: false,  //是否滚动了。
+    mainurl : '',
+    IsNext: false,//是否有下一页,
+    pageIndex:1,
+    recommendList:[],
+    group:20,
+    listlenth:0
+  },
+  getInfo() {
+    var that = this
+    app.ajax({
+      method: 'get',
+      url: app.mainUrl + 'api/Home/WorkHome',
+      data: { pageIndex: Number(that.data.pageIndex), pageSize: that.data.group, Keyword:-1},
+      success: function (res) {
+        wx.hideLoading()
+        if (res.data.Status == 1) {
+          if (that.data.pageIndex == 1) {
+            that.data.jobList = [];
+            that.setData({
+              jobList1: res.data.Result.result.Works,
+            })
+          } else if (that.data.pageIndex == 2){
+            console.log('111')
+            that.setData({
+              jobList2: res.data.Result.result.Works,
+            })
+          } else if (that.data.pageIndex == 3) {
+            that.setData({
+              jobList3: res.data.Result.result.Works,
+            })
+          } else if (that.data.pageIndex == 4) {
+            that.setData({
+              jobList4: res.data.Result.result.Works
+            })
+          } else if (that.data.pageIndex > 4) {
+            that.data.jobList4 = that.data.jobList4.concat(res.data.Result.result.Works)
+            that.setData({
+              jobList4: that.data.jobList4,
+            })
+          }
+          that.data.pageIndex = that.data.pageIndex + 1
+          that.data.listlenth = res.data.Result.result.Works.length
+          that.setData({
+            // imgUrls: [
+            //   app.mainUrl + res.data.Result.BannerImage
+            // ],
+            recommendList: res.data.Result.result,
+            IsNext: res.data.Result.IsNext
+          })
+        } else {
+          wx.showModal({
+            showCancel: false,
+            title: '提示',
+            content: res.data.Result,
+          })
+        }
+
+      },
+      error: function () {
+        wx.hideLoading()
+      }
+    })
   },
   // 点击跳转详情页
   jobDetail(event) {
@@ -45,20 +89,143 @@ Page({
       url: 'jobDetail/jobDetail?id=' + id,
     })
   },
-  // 点击搜素
-  changeSearch(e) {
-    this.setData({
-      searchVal: e.detail.value
-    })
-  },
+
   gotosearch(){
     wx.navigateTo({
       url: 'search/search'
     })
   },
-  recommend(){
-    wx.navigateTo({
-      url: '../money/recommend/recommend'
+  
+  /**
+ * 生命周期函数--监听页面加载
+ */
+onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.setData({
+      pageIndex: 1
     })
+    this.getInfo();
+    this.mainurl = app.mainUrl
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    var that = this
+    this.setData({
+      pageIndex: 1
+    })
+    var that = this
+    app.ajax({
+      method: 'get',
+      url: app.mainUrl + 'api/Home/WorkHome',
+      data: { pageIndex: Number(that.data.pageIndex), pageSize: that.data.group, Keyword: -1 },
+      success: function (res) {
+        wx.hideLoading();
+        wx.hideNavigationBarLoading() //完成停止加载
+        wx.stopPullDownRefresh() //停止下拉刷新
+        if (res.data.Status == 1) {
+          if (that.data.pageIndex == 1) {
+            that.data.jobList = [];
+            that.setData({
+              jobList1: res.data.Result.result.Works,
+            })
+          } else if (that.data.pageIndex == 2) {
+            that.setData({
+              jobList2: res.data.Result.result.Works,
+            })
+          } else if (that.data.pageIndex == 3) {
+            that.setData({
+              jobList3: res.data.Result.result.Works,
+            })
+          } else if (that.data.pageIndex == 4) {
+            that.setData({
+              jobList4: res.data.Result.result.Works
+            })
+          } else if (that.data.pageIndex > 4) {
+            that.data.jobList4 = that.data.jobList4.concat(res.data.Result.result.Works)
+            that.setData({
+              jobList4: that.data.jobList4,
+            })
+          }
+          that.data.pageIndex = that.data.pageIndex + 1
+          that.data.listlenth = res.data.Result.result.Works.length
+          that.setData({
+            // imgUrls: [
+            //   app.mainUrl + res.data.Result.BannerImage
+            // ],
+            recommendList: res.data.Result.result,
+            IsNext: res.data.Result.IsNext
+          })
+        } else {
+          wx.showModal({
+            showCancel: false,
+            title: '提示',
+            content: res.data.Result,
+          })
+        }
+
+      },
+      error: function () {
+        wx.hideLoading()
+      }
+    })
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    if (this.data.IsNext) {
+      var that = this
+      if (that.data.pageIndex == 1) {
+        that.setData({
+          pageIndex: 2
+        })
+      }
+      that.getInfo()
+    } else {
+      wx.showToast({
+        title: '没有更多数据了',
+      })
+    }
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
+
