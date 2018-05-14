e180001b59511ccc40a08737ca99124377e751af
zjy <296579033@qq.com>
2018-05-14 17:35:52 +0800

    意见反馈接口完成

--- pages/mine/feedback/feedback.js
+++ pages/mine/feedback/feedback.js
@@ -1,11 +1,54 @@
 // pages/mine/feedback/feedback.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    Text:''
+  },
+  Changeinput(e){
+    this.setData({
+      Text: e.detail.value
+    });
+  },
+  save(){
+    if (this.data.Text == ""){
+      wx.showModal({
+        showCancel: false,
+        title: '提示',
+        content: '请输入反馈内容',
+      })
+      return;
+    }
+    var tt = this;
+    app.ajax({
+      method: 'get',
+      url: app.mainUrl + 'api/Home/UserFeedBack',
+      data: {
+        'Content': tt.data.Text
+      },
+      success: function (res) {
+        wx.hideLoading()
+        if (res.data.Status == 1) {
+          wx.showToast({
+            title: res.data.Result
+          })
+        } else {
+          wx.showModal({
+            showCancel: false,
+            title: '提示',
+            content: res.data.Result,
+          })
+        }
+      },
+      error: function () {
+        wx.navigateTo({
+          url: '../../login/login'
+        })
+      }
+    })
   },
 
   /**
